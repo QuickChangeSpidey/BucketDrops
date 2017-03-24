365764611f820453f00ca535d1dabb2b78fda193
akshayLemonaid <akshay@polkadoc.com>
2017-03-24 15:21:44 -0700

    first screen button works

--- app/src/main/java/com/akshay/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/akshay/bucketdrops/ActivityMain.java
@@ -3,17 +3,45 @@ package com.akshay.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class ActivityMain extends AppCompatActivity{
 
     Toolbar mToolbar;
+    Button mButton;
+
+    private View.OnClickListener mButtonPressListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Toast.makeText(getApplicationContext(),"Button Clicked", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mButton = (Button) findViewById(R.id.btn_add);
+        mButton.setOnClickListener(mButtonPressListener);
         setSupportActionBar(mToolbar);
+        initBackgroundImage();
     }
+
+    private void initBackgroundImage() {
+
+        ImageView imageView = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(imageView);
+
+    }
+
 }
