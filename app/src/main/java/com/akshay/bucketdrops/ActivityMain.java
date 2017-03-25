f5aa56d790e0a776370f64b198cdab3a4e3bb250
akshayLemonaid <akshay@polkadoc.com>
2017-03-24 17:32:26 -0700

    butterknife code added and dependency fixed

--- app/src/main/java/com/akshay/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/akshay/bucketdrops/ActivityMain.java
@@ -6,8 +6,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
